63d7034611974a3e35141f501026408380dd8370
Carla Pena <exoreiba@gmail.com>
2023-09-29 15:53:31 +0200

    seccion de de eventos en el view

--- public/index.php
+++ public/index.php
@@ -55,11 +55,12 @@ $router->post('/admin/ponentes/eliminar', [PonentesController::class, 'eliminar'
 $router->get('/admin/eventos', [EventosController::class, 'index']);
 $router->get('/admin/eventos/crear', [EventosController::class, 'crear']);
 $router->post('/admin/eventos/crear', [EventosController::class, 'crear']);
+$router->get('/admin/eventos/editar', [EventosController::class, 'editar']);
+$router->post('/admin/eventos/editar', [EventosController::class, 'editar']);
 
 
 //API HORARIOS
 $router->get('/api/eventos-horario', [APIEventos::class, 'index']);
-
 
 //API ponentes
 $router->get('/api/ponentes', [APIPonentes::class, 'index']);
